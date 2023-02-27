f60e2f32bd91f135c2fa6fa393bec4edb8b96b62
RushaliBhudeka <126426949+RushaliBhudeka@users.noreply.github.com>
2023-02-26 21:47:46 -0600

    Update FETCH_Assesment_RB.sql

--- FETCH_Assesment_RB.sql
+++ FETCH_Assesment_RB.sql
@@ -3,14 +3,14 @@
 SELECT TOP 1 [USER_ID],SUM(TOTAL_SPENT) AS MOST_SPENT FROM DBO.RECEIPTS
 WHERE MONTH([PURCHASE_DATE])=8
 GROUP BY [USER_ID]
-ORDER BY SUM(TOTAL_SPENT) DESC
+ORDER BY SUM(TOTAL_SPENT) DESC;
 
 ---------------------------------------------------------------------------------------------------------------------------
 
 --How many users scanned in each month?
 SELECT MONTH([DATE_SCANNED]) AS MONTHS,COUNT(DISTINCT [USER_ID]) AS NO_OF_USERS_SCANNED_PER_MONTH
 FROM [dbo].[receipts]
-group by MONTH([DATE_SCANNED])
-ORDER BY MONTH([DATE_SCANNED]) 
+GROUP by MONTH([DATE_SCANNED])
+ORDER BY MONTH([DATE_SCANNED]);
 
 ---------------------------------------------------------------------------------------------------------------------------
